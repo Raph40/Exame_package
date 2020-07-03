07ed1d18a4a9b630cd7db170627cfc4732f0d48b
Raph40 <66115952+Raph40@users.noreply.github.com>
2020-07-03 12:36:02 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+	version='1.0',
+	descriptation='Hello Discriptation Module',
+	author='Rafael'
+	author_email='rafajoselopes@gmail.com'
+	url='https://github.com/Raph40/Exame_package.git'
+	py_module=['Exame_package']
+	)
